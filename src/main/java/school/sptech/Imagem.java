7666faf438e0e64496b2f21cfda88a210a147769
Gustavo-Kohatsu <gustavo.kohatsu@sptech.school>
2024-11-07 17:59:13 -0300

    finalizado

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,54 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras;
+
+    public Imagem() {
+
+    }
+
+    public Imagem(List<Figura> figuras) {
+        this.figuras = new ArrayList<>();
+    }
+
+    public void adicionar(Figura figura) {
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas() {
+        Double somaTotalAreas = 0.0;
+
+        for (Figura figura : figuras) {
+            somaTotalAreas += figura.calcularArea();
+        }
+
+        return somaTotalAreas;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20() {
+        List<Figura> figurasMaiorQue20 = new ArrayList<>();
+
+        for (Figura figura : figuras) {
+            if (figura.calcularArea() > 20.0) {
+                figurasMaiorQue20.add(figura);
+            }
+        }
+
+        return figurasMaiorQue20;
+    }
+
+    public List<Figura> buscarQuadrados() {
+        List<Figura> figurasQuadrados = new ArrayList<>();
+
+        for (Figura figura : figuras) {
+            if (figura instanceof Quadrado) {
+                figurasQuadrados.add(figura);
+            }
+        }
+
+        return figurasQuadrados;
+    }
 }
